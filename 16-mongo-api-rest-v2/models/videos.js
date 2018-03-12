809c50b80c33c9a22a59fb2eee8dd255b8243a13
moli612 <lauramolinojimenez@gmail.com>
2018-03-12 16:33:08 +0100

    modificacion rutas videos

--- 16-mongo-api-rest-v2/models/videos.js
+++ 16-mongo-api-rest-v2/models/videos.js
@@ -3,15 +3,16 @@ var uniqueValidator = require('mongoose-unique-validator');
 
 
 var VideoSchema = new mongoose.Schema({
+    _id:String,
     title:String,
     comments:String,
     rating:Number,
     views:Number,
     label:String,
     date:Date,
-    owner:String,
+    owner:String
 
-}, {timestamps: true,collection:"user"});
+}, {timestamps: true,collection:"videos"});
 
 
 var video = mongoose.model("Video", VideoSchema);
--- 16-mongo-api-rest-v2/routes/videos.js
+++ 16-mongo-api-rest-v2/routes/videos.js
@@ -1,71 +1,260 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://192.168.99.100/test');
+var db = mongoose.connection;
+var conectado = false;
+var videos = require("../models/videos");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("Conexion abierta");
+    conectado = true;
 
-router.get('/', function (req, res, next){
-    var objetoJSON={propiedad:'valor'};
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(objetoJSON));
 });
+var fs = require('fs');
 
-router.get('/', function (req, res, next){
-    res.send('Listado de videos');
-    var videos=[{
-        title:"Video 1",
-        comments:"o",
-        url:"o",
-        rating:"o",
-        views:"1",
-        label:"o",
-        date:"o",
-        owner:"Usuario 1"
-    },{
-        title:"Video 2",
-        comments:"e",
-        url:"e",
-        rating:"e",
-        views:"40",
-        label:"e",
-        date:"e",
-        owner:"Usuario 2"
-    },{
-        title:"Video 3",
-        comments:"a",
-        url:"a",
-        rating:"a",
-        views:"55",
-        label:"a",
-        date:"a",
-        owner:"Usuario 3"
-    }];
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(videos));
+function cogeLogin(session){
+    return session.videos;
+}
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    /*res.render('api', {
+        title: 'API Rest Mongo'
+    });*/
+     //res.send('Listado de comentarios');
+    if (conectado) {
+        res.setHeader('Content-Type', 'application/json');
+        Comentario.find(function (err,videos) {
+            if (err) return console.error(err);
+            //console.log(users);
+            res.send(JSON.stringify(videos));
+        });
+
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
 });
+router.get('/get/:id', function (req, res, next) {
+    //console.log(req.params.id);
+    if (conectado) {
+        var objeto = {
 
-router.post('/', function (req, res, next){
-    var addVideo
-    res.send('Añadir un video ');
+        };
+        objeto._id = req.params.id;
+        User.findOne(
+            //{_id:req.params.id}
+            objeto,
+            function (err, videos) {
+                if (err) return console.error(err);
+                //console.log(users);
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(videos));
+            }
+        );
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
+
 });
+router.get('/edita/:id', function (req, res, next) {
+    //console.log(req.params.id);
+    if (conectado) {
+        var objeto = {
 
-router.get('/:id/', function (req, res, next){
-    var libros={};
-    res.send('Mostrar un video '+ req.params.id);
+        };
+        objeto._id = req.params.id;
+        User.findOne(
+            objeto,
+            function (err, videos) {
+                if (err) return console.error(err);
+                //console.log(users);
+                res.render("edit", {
+                    item: videos
+                });
+            }
+        );
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
+
 });
+router.post('/edit/:id', function (req, res, next) {
+    //console.log(req.params.id);
+    if (conectado) {
+        var objetoModificado = {};
+        objetoModificado._id = req.params.id;
+        objetoModificado.title = req.body.title;
+        objetoModificado.comments = req.body.comments;
+        objetoModificado.rating = req.body.rating;
+        objetoModificado.views = req.body.views;
+        objetoModificado.label = req.body.label;
+        objetoModificado.date = req.body.date;
+        objetoModificado.owner = req.body.owner;
+        console.log(objetoModificado);
+        User.findByIdAndUpdate(
+            req.params.id,
+            objetoModificado,
+            function (err, videos) {
+                if (err) return console.error(err);
+                console.log(videos);
+                User.findById(
+                    req.params.id,
+                    function (err, videos) {
+                        if (err) return console.error(err);
+                        console.log(videos);
+                        res.setHeader('Content-Type', 'application/json');
+                        res.send(JSON.stringify(videos));
+                    }
+                );
+            }
+        );
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
 
-router.post('/:id/', function (req, res, next){
-    res.send('Editar un video ' + req.params.id);
 });
+router.get('/borra/:id', function (req, res, next) {
+    //console.log(req.params.id);
+    if (conectado) {
+        var objeto = {
 
-router.get('/delete/:id/', function (req, res, next){
-    res.send('Borrar un video ' + req.params.id);
+        };
+        objeto._id = req.params.id;
+        User.findById(
+            objeto,
+            function (err, videos) {
+                if (err) return console.error(err);
+                //console.log(users);
+                res.render('borra', {
+                    item: videos
+                });
+            }
+        );
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
+
 });
+router.get('/delete/:id', function (req, res, next) {
+    //console.log(req.params.id);
+    if (conectado) {
+        User.findByIdAndRemove(
+            req.params.id,
+            function (err, videos) {
+                if (err) return console.error(err);
+                //console.log(users);
+                //FALTA BORRAR
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(videos));
+            }
+        );
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
 
-router.post('/upload/', function (req, res, next){
-    res.send('Subir un video ');
+});
+router.get('/show/:id', function (req, res, next) {
+    console.log(req.params.id);
+    if (conectado) {
+        var objeto = {
+
+        };
+        objeto._id = req.params.id;
+        User.findOne(
+            objeto,
+            function (err, videos) {
+                if (err) return console.error(err);
+                //console.log(users);
+                res.render('show', {
+                    item: videos
+                });
+            }
+        );
+
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
+
+});
+/*router.get('/addForm', function (req, res, next) {
+    if (conectado) {
+        res.render('videos', {
+            title: 'API Rest Mongo'
+        });
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
+
+});*/
+/*router.get('/addStatic', function (req, res, next) {
+
+    if (conectado) {
+        var video = new Video({
+            title: ""
+            //, otro_campo:"otro valor"
+        });
+        video.save(function (err, videodevuelto) {
+            if (err) {
+                return console.error(err);
+            } else {
+                console.log("video guardado");
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(videodevuelto));
+            }
+        });
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
+
+});*/
+router.post('/add', function (req, res, next) {
+    if (conectado) {
+        console.log(req.body);
+        var video = new Video({
+            username: req.body.nombre,
+            hash: req.body.pass,
+            title = req.body.title,
+        });
+        usuario.setPassword(req.body.pass);
+        usuario.save(function (err, userdevuelto) {
+            if (err) {
+                return console.error(err);
+            } else {
+                console.log("usuario guardado");
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(userdevuelto));
+            }
+        });
+    } else {
+        res.render('errorDB', {
+            title: 'Mongo No arrancado'
+        });
+    }
+
+});
+router.get("/uploadVideoGet",function(req,res){
+    console.log("presentando Video");
+    res.render("upload", {
+        title: 'Subir Video'
+    });
 });
 
 module.exports = router;
-//mostrar los creo yo
-//añadir y editar son parecidos 
-//el listado devuelve un listado de objetos json 
-//borrar devolver un objeto
-//subir a express
